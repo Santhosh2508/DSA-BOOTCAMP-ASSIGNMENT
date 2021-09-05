9bcabc92bca676b13c985975efc0f72ed08fc604
Santhosh2508 <90123284+Santhosh2508@users.noreply.github.com>
2021-09-05 10:21:08 +0530

    Q5.Program to check whether a number is Prime.
    Q5. Write a program to check whether a number is Prime or Not.

---
+++ Q5.Prime.cpp
@@ -0,0 +1,28 @@
+/*Write a program to check whether a number is Prime or Not.*/
+#include <iostream>
+using namespace std;
+int Prime(int n);
+
+int Prime(int n)
+{
+    int i;
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    int num, result;
+    cout << "Enter the Number: ";
+    cin >> num;
+    result = Prime(num);
+    if (result == 1)
+        cout << num << " is a Prime number" << endl;
+    else
+        cout << num << " is not Prime number" << endl;
+    return 0;
+}
