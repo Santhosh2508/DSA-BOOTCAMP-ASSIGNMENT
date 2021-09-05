f937977904d94e4f37df34bd30245a42e526f3a6
Santhosh2508 <90123284+Santhosh2508@users.noreply.github.com>
2021-09-05 10:18:19 +0530

    Q1. Write a program to Swap to two numbers.
    Q1. Write a program to Swap to two numbers.

---
+++ Q1Swap_b.cpp
@@ -0,0 +1,17 @@
+/* Write a program to swap to two numbers*/
+/*b. By without using third variable*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, temp;
+    cout << "Enter the two numbers: ";
+    cin >> a >> b;
+    cout << "Before Swaping : a =" << a << " and b = " << b << endl;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "After Swaping : a =" << a << " and b = " << b << endl;
+    return 0;
+}
