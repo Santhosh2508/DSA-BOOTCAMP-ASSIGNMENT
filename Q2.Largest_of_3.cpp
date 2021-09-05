756c81005e048803e1a15bdb3c8df8798e16d2cf
Santhosh2508 <90123284+Santhosh2508@users.noreply.github.com>
2021-09-05 10:18:56 +0530

    Q2.Program to find the largest number
    Q2. Write a program to find the largest number among three numbers entered by the user.

---
+++ Q2.Largest_of_3.cpp
@@ -0,0 +1,17 @@
+/*Write a program to find the largest number among three numbers entered by the user*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    int largest;
+    cout<<"Enter the three numbers: "<<endl;
+    cin>>a>>b>>c;
+    largest = a;                             //Assume first number is largest
+    if(b>largest)
+    largest = b;
+    if(c>largest)
+    largest=c;
+    cout<<"Largest Number is = "<<largest<<endl;
+    return 0;
+}
